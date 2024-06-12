3935a4857c4a4b2743c80b564957880a25e489ec
Shubham Bisht <shubham99bisht@gmail.com>
2024-06-12 16:46:40 +0530

    Program Modules

--- firebase/program_types.js
+++ firebase/program_types.js
@@ -6,21 +6,39 @@ const modalFooter = addNewProgramModel.querySelector('.modal-footer')
 
 // Modal's form inputs
 const nameInput = addNewProgramModel.querySelector('.modal-body #program-name')
+const modulesInput = addNewProgramModel.querySelector('#modulesInput')
 
 let programs = {}
+
+/**
+ * --------------------------------------------------
+ * Modal
+ * --------------------------------------------------
+ */
 
 if (addNewProgramModel) {
   addNewProgramModel.addEventListener('show.bs.modal', event => {
     // Button that triggered the modal
     const button = event.relatedTarget
     const row = button.closest('tr')
-    // Extract info from data-bs-* attributes
-    const name = row?.querySelector('.name')
+    const program = programs[row?.id]
+
+    modulesInput.innerHTML = `<label class="form-label mb-0">Modules</label><div class="form-text mb-3">Empty modules will be deleted automatically.</div>`
 
     // Update the modal's content.
-    if (name) {
+    if (program) {
       modalTitle.textContent = `Update Program`
-      nameInput.value = name.innerHTML.trim()
+      nameInput.value = program['name'].trim()
+
+      program['modules']?.forEach(text => {
+        const input = document.createElement('input');
+        input.setAttribute('class', 'form-control mb-2');
+        input.setAttribute('type', 'text');
+        input.setAttribute('value', text);
+        input.setAttribute('required', 'required');
+        modulesInput.appendChild(input);
+      });
+
       modalFooter.innerHTML = `
         <button class="btn btn-secondary me-2" type="button" data-bs-dismiss="modal">Cancel</button>
         <button class="btn btn-warning" onclick="updateProgram('${row?.id}')">Update</button>`
@@ -34,9 +52,26 @@ if (addNewProgramModel) {
   })
 }
 
-window.onload = function() {
-  listAllProgramTypes();
-};
+function addNewModule() {
+  const input = document.createElement('input');
+  input.setAttribute('class', 'form-control mb-2');
+  input.setAttribute('type', 'text');
+  input.setAttribute('placeholder', 'Module Name');
+  input.setAttribute('required', 'required');
+  modulesInput.appendChild(input);  
+}
+window.addNewModule = addNewModule
+
+function readAllModules() {
+  const inputs = modulesInput.querySelectorAll('input[type="text"]') || [];
+  const values = [];
+  
+  inputs?.forEach(input => {
+    if (input.value) values.push(input.value);
+  });
+  
+  return values;
+}
 
 /**
  * --------------------------------------------------
@@ -46,7 +81,8 @@ window.onload = function() {
 
 function createProgram() {
   const newProgramType = {
-    name: nameInput.value
+    name: nameInput.value,
+    modules: readAllModules()
   };
   if (!newProgramType.name) {
     failMessage("Please provide valid program name"); return
@@ -83,6 +119,7 @@ function listAllProgramTypes() {
       const schema = `
         <tr id="{}">
           <td class="name">{}</td>
+          <td class="modules">{}</td>
           <td class="align-middle white-space-nowrap py-2 text-end">
             <a data-bs-toggle="modal" data-bs-target="#add-new-modal" style="cursor:pointer">
               <i class="fas fa-edit"></i>
@@ -90,13 +127,13 @@ function listAllProgramTypes() {
           </td>
         </tr>`
 
-      let csvContent = 'Program Name\r\n';
+      let csvContent = 'Program Name,Module Count\r\n';
 
       Object.keys(programTypes).forEach(pId => {
         try {
           const p = programTypes[pId]
-          const row = schema.format(pId, p.name, pId)
-          csvContent += `${p.name}\r\n`
+          const row = schema.format(pId, p.name, p?.modules?.length || 0, pId)
+          csvContent += `${p.name},${p?.modules?.length || 0}\r\n`
           if (tableBody) tableBody.innerHTML += row
         } catch {}
       });
@@ -120,7 +157,8 @@ window.listAllProgramTypes = listAllProgramTypes
 
 function updateProgram(pId) {
   const updatedProgramType = {
-    name: nameInput.value
+    name: nameInput.value,
+    modules: readAllModules()
   };
 
   if (!updatedProgramType.name) {
@@ -165,3 +203,9 @@ async function deleteProgram(pId) {
   }
 }
 window.deleteProgram = deleteProgram
+
+
+
+window.onload = function() {
+  listAllProgramTypes();
+};
